e0d90f8b7009add5c01f0720b241c7ee7789a856
Luiz <luiz.ado@hotmail.com>
2020-10-15 20:27:37 -0300

    Added @unform/core and @unform/web libs to the project.

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -8,13 +9,17 @@ import logoImg from '../../assets/logoImg.svg';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const handleSubmit = (data: object): void => {
+    console.log(data);
+  };
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="Gobarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
@@ -27,7 +32,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="/login">
           <FiArrowLeft />
