f1424c03474c0f05b810c21412e226a2eecb87e3
Luiz <luiz.ado@hotmail.com>
2020-10-14 23:46:07 -0300

    Added SignUp page to the application.

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logoImg.svg';
+
+import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  return <div>SingUp</div>;
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="Gobarber" />
+
+        <form>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+          <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <a href="/login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
 };
 
 export default SignUp;
--- src/Pages/SignUp/styles.ts
+++ src/Pages/SignUp/styles.ts
@@ -1,67 +1,54 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImg from '../../assets/signInBackgroundImg.png';
+import signUpBackgroundImage from '../../assets/signUpBackgroundImg.png';
 
 export const Container = styled.div`
-  height: 100vh;
   display: flex;
-
+  height: 100vh;
   align-items: stretch;
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-
-  align-items: center;
   place-content: center;
+  align-items: center;
+
   width: 100%;
   max-width: 700px;
 
   form {
-    margin: 80px 0;
+    margin: 80px 0px;
     width: 340px;
     text-align: center;
 
     h1 {
       margin-bottom: 24px;
     }
-
-    a {
-      color: #f4ede8;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
-
-      &:hover {
-        color: ${shade(0.2, '#f4ede8')};
-      }
-    }
   }
 
-  > a {
-    color: #ff9000;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-
+  a {
     display: flex;
     align-items: center;
+    color: #f4ede8;
+    margin-top: 24px;
+    text-decoration: none;
+
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
 
     svg {
       margin-right: 16px;
-    }
-
-    &:hover {
-      color: ${shade(0.2, '#ff9000')};
     }
   }
 `;
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signInBackgroundImg}) no-repeat center;
+  background: url(${signUpBackgroundImage}) no-repeat center;
   background-size: cover;
 `;
