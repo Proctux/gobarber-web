312bcf353c8c19edd76ef38c04308fceea377352
Luiz <luiz.ado@hotmail.com>
2020-10-14 22:22:21 -0300

    Created the SignIn page and added some images and libs to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 
+import SignIn from './Pages/SignIn';
+// import SignUp from './Pages/SignUp';
+
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <SignIn />
       <GlobalStyle />
     </>
   );
