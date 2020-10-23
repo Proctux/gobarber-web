3418965023aa09bbbe79beec8b48a39b37d03d85
Luiz <luiz.ado@hotmail.com>
2020-10-22 23:40:51 -0300

    Changed Dashboard layout. Fix the proptypes of user on 'auth.tsx' file.

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import Button from '../../components/Button';
+import { FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Content } from './styles';
+import { Container, HeaderContent, Header, Profile } from './styles';
+
+import logoImg from '../../assets/logoImg.svg';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  console.log(user.avatar_url);
 
   return (
     <Container>
-      <Content>
-        <h1>Hello User!!</h1>
-        <Button type="button" onClick={signOut}>
-          Sair da Aplicação
-        </Button>
-      </Content>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button onClick={signOut} type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 };
