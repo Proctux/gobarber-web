ea83dfcbf114bda7d0e0536a416258982974c0e1
Luiz <luiz.ado@hotmail.com>
2020-10-31 12:19:09 -0300

    Added Profile page, adjusted some information on Input Component and Auth hook. Added the new page to routes and set the navigation from Dashboard to it.

---
+++ src/Pages/Profile/index.tsx
@@ -0,0 +1,128 @@
+import React, { useCallback, useRef } from 'react';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import { Link, useHistory } from 'react-router-dom';
+import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
+import api from '../../services/api';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useToast } from '../../hooks/toast';
+import { useAuth } from '../../hooks/auth';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, AvatarInput } from './styles';
+
+interface ProfileFormData {
+  name: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const { user } = useAuth();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string(),
+          password: Yup.string().required(),
+          password_confirmation: Yup.string().required(),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.put('/profile', data);
+
+        addToast({
+          title: 'Perfil Atualizado com sucesso',
+          type: 'success',
+          description: 'Seu perfil foi atualizado com sucesso',
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro ao atualizar o perfil',
+          description:
+            'Aconteceu um erro ao tentar atualizar seu perfil. Tente novamente mais tarde.',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+      <Content>
+        <Form
+          ref={formRef}
+          initialData={{
+            name: user.name,
+            email: user.email,
+          }}
+          onSubmit={handleSubmit}
+        >
+          <AvatarInput>
+            <img src={user.avatar_url} alt={user.name} />
+            <button type="button">
+              <FiCamera />
+            </button>
+          </AvatarInput>
+
+          <h1>Meu Perfil</h1>
+
+          <Input name="name" type="text" icon={FiUser} placeholder="Nome" />
+          <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            name="old_password"
+            type="password"
+            icon={FiLock}
+            placeholder="Senha atual"
+          />
+          <Input
+            name="password"
+            type="password"
+            icon={FiLock}
+            placeholder="Nova senha"
+          />
+          <Input
+            name="password_confirmation"
+            type="password"
+            icon={FiLock}
+            placeholder="Confirmação Senha"
+          />
+
+          <Button type="submit">Confirmar mudanças</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
